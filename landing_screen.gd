ca9ea61b291f74b540be1266e7743b55b854a4de
safonovoplatit <sazonovpeters@gmail.com>
2025-07-05 23:03:46 +0300

    js

--- landing_screen.gd
+++ landing_screen.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var main_scene = preload("res://main.tscn").instantiate()
+#var main_scene = preload("res://main.tscn").instantiate()
 
 func _on_texture_button_pressed() -> void:
-	get_tree().root.add_child(main_scene)
+	get_tree().change_scene_to_file("res://main.tscn")
